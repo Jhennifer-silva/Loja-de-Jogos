36f50c0a9661c55fc9fe59ab7401c6cdacd4114c
Jhennifer-Silva <jhennifer.silva@estudante.ufla.br>
2025-01-09 00:40:30 -0300

    Tela da conta e Navbar responsivas

--- lojaDeJogos/src/Telas/conta/Conta.jsx
+++ lojaDeJogos/src/Telas/conta/Conta.jsx
@@ -6,27 +6,41 @@ import jogo3 from '../../assets/conta/jogo3.png';
 import jogo4 from '../../assets/conta/jogo4.png';
 import carrinho from'../../assets/carrinho.png';
 import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import '../../styles.css';
 import './conta.css';
-import '../../styles.css'
 
 function Conta({dadosUsuario}) {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(prevState => !prevState);
+        document.body.classList.toggle('no-scroll', !showMenu)
+    };
+    
     return (
         <div className="telaConta">
-            <nav className="navbar">
+            <nav className={`navbar ${showMenu ? 'on' : ''}`}>
                 <Link to="/">
                     <img src={icone} alt="Icone do site" className="icone" />
                 </Link>
                 <ul className="nav-links">
-                <a href="principal#lancamentos">Lançamentos</a> 
-                <a href="/populares">Populares</a> 
-                <a href="#">Gêneros</a> 
-                <a href="principal#promocoes">Promoções</a> 
-                <a href="/conta">Conta</a>
+                    <a href="principal#lancamentos">Lançamentos</a> 
+                    <a href="/populares">Populares</a> 
+                    <a href="#">Gêneros</a> 
+                    <a href="principal#promocoes">Promoções</a> 
+                    <a href="/conta">Conta</a>
                 </ul>
                 <div className="pesquisa-container">
                     <input type="text" id="pesquisa"/>
                     <i className="fa fa-search"></i>
                 </div>
+                    <div className="menu-sand" onClick={toggleMenu}>
+                        <div className="menuUm"></div>
+                        <div className="menuDois"></div>
+                        <div className="menuTres"></div>
+                    </div>
             </nav>
             <div className="jogos">
                 <p id="titulo">Jogos adquiridos:</p>
@@ -54,13 +68,16 @@ function Conta({dadosUsuario}) {
                 <h2>Olá, {dadosUsuario.nome}</h2>
                 <p>Seu email é {dadosUsuario.email}</p>
                 <p>Seu CPF é {dadosUsuario.cpf}</p>
-                <button>Alterar dados <i className="fas fa-edit"></i></button>
-                <button>Excluir Conta <i className="fas fa-trash-alt"></i></button>
-                <button id="admin">Tela do admin </button>
+                <div className="configsButtons">
+                    <button>Alterar dados <i className="fas fa-pen"></i></button>
+                    <button>Excluir Conta <i className="fas fa-trash-alt"></i></button>
+                    <button id="admin">Tela do admin </button>
+                </div>
             </div>
 
         </div>
     )
+
 }
 
 export default Conta;
